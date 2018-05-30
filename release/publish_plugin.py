cf4ed1abef715e8f46c37058348752878d209e0d
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-05-29 21:31:59 -0400

    switch to python3

--- release/publish_plugin.py
+++ release/publish_plugin.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding=utf-8
 """This script uploads a plugin package to the repository.
         Authors: A. Pasotti, V. Picavet
         git sha              : $TemplateVCSFormat
 """
-from __future__ import print_function
-from future import standard_library
 
 import sys
 import getpass
@@ -14,8 +12,6 @@ import json
 import http.client
 import os
 from optparse import OptionParser
-
-standard_library.install_aliases()
 
 # Configuration
 PROTOCOL = 'https'
